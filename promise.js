3c24a5d609d1f018922b7e8abc7c2a40fa9fb44d
liquan <chnliquan@163.com>
2018-02-07 20:07:52 +0800

    @dev 根据标准添加延时函数

--- promise.js
+++ promise.js
@@ -7,34 +7,42 @@ const Promise = (()=>{
     const Promise = function (_executor) {
         this.status = 'pending'; // 初始状态
         this.data = void 0; // 初始值
-        this.callbacks = []; // resolve reject 回调函数集合
+        this.callbacks = []; // onResolved onRejected 回调函数集合
 
         // 成功回调执行函数
         const _resolve = (_value) => {
-            // 如果当前状态不是 pending 说明状态已经确定，不能再次改变
-            if (this.status !== PENDING) return;
+            // Notes 3.1
+            setTimeout(() => {
+                // 如果当前状态不是 pending 说明状态已经确定，不能再次改变
+                if (this.status !== PENDING) return;
 
-            this.status = FULFILLED;
-            this.data = _value;
+                // 设置最终状态为 fulfilled
+                this.status = FULFILLED;
+                this.data = _value;
 
-            // 触发集合中的 onResolved 函数
-            for(let i = 0, l = this.callbacks.length; i < l; i++) {
-                this.callbacks[i].onResolved(_value);
-            }
+                // 触发集合中的 onResolved 函数
+                for(let i = 0, l = this.callbacks.length; i < l; i++) {
+                    this.callbacks[i].onResolved(_value);
+                }
+            })
         };
 
         // 失败回调执行函数
         const _reject = (_reason) => {
-            // 如果当前状态不是 pending 说明状态已经确定，不能再次改变
-            if (this.status !== PENDING) return;
+            // Notes 3.1
+            setTimeout(() => {
+                // 如果当前状态不是 pending 说明状态已经确定，不能再次改变
+                if (this.status !== PENDING) return;
 
-            this.status = REJECTED;
-            this.data = _reason;
+                // 设置最终状态为 rejected
+                this.status = REJECTED;
+                this.data = _reason;
 
-            // 触发集合中的 onRejected 函数
-            for(let i = 0, l = this.callbacks.length; i < l; i++) {
-                this.callbacks[i].onRejected(_reason);
-            }
+                // 触发集合中的 onRejected 函数
+                for(let i = 0, l = this.callbacks.length; i < l; i++) {
+                    this.callbacks[i].onRejected(_reason);
+                }
+            })
         };
 
         // 防止 executor 函数中发生异常
@@ -48,43 +56,48 @@ const Promise = (()=>{
     Promise.prototype.then = function(_onResolved, _onRejected) {
         let _promise2;
 
+        // 2.2.1
         _onResolved = typeof _onResolved === 'function' ?
-                      _onResolved : function(_value) {return _value};
+                      _onResolved : (_value) => _value;
         _onRejected = typeof _onRejected === 'function' ?
-                      _onRejected : function(_reason) {throw _reason};
+                      _onRejected : (_reason) => {throw _reason};
 
         if (this.status === FULFILLED) {
             return _promise2 = new Promise((_resolve, _reject) => {
-                try {
-                    const _x = _onResolved(this.data);
+                setTimeout(() => {
+                    try {
+                        const _x = _onResolved(this.data); // _x 可能为一个 thenable
 
-                    _resolvePromise(_promise2, _x, _resolve, _reject);
-                }catch (_e) {
-                    return _reject(_e);
-                }
+                        _resolvePromise(_promise2, _x, _resolve, _reject);
+                    }catch (_e) {
+                        return _reject(_e);
+                    }
+                })
             });
         }
 
         if (this.status === REJECTED) {
             return _promise2 = new Promise((_resolve, _reject) => {
-                try {
-                    const _x = _onRejected(this.data);
+                setTimeout(() => {
+                    try {
+                        const _x = _onRejected(this.data); // _x 可能为一个 thenable
 
-                    _resolvePromise(_promise2, _x, _resolve, _reject);
-                }catch (_e) {
-                    return _reject(_e);
-                }
+                        _resolvePromise(_promise2, _x, _resolve, _reject);
+                    }catch (_e) {
+                        return _reject(_e);
+                    }
+                })
             });
         }
 
         if (this.status === PENDING) {
             return _promise2 = new Promise((_resolve, _reject) => {
                 // 如果当前状态是 pending，此时不能确定调用 onResolved 还是 onRejected
-                // 将处理逻辑做为 callback 放入 promise1 的回调函数集合里，当状态确定时触发对应的回调
+                // 将处理逻辑做为 callback 放入 promise 的回调函数集合里，当状态确定时触发对应的回调
                 this.callbacks.push({
                     onResolved (_value) {
                         try {
-                            const _x = _onResolved(_value);
+                            const _x = _onResolved(_value); // _x 可能为一个 thenable
                             
                             _resolvePromise(_promise2, _x, _resolve, _reject);
                         } catch (_e) {
@@ -94,7 +107,7 @@ const Promise = (()=>{
 
                     onRejected (_reason) {
                         try {
-                            const _x = _onRejected(_reason);
+                            const _x = _onRejected(_reason); // _x 可能为一个 thenable
                             
                             _resolvePromise(_promise2, _x, _resolve, _reject);
                         } catch (_e) {
@@ -113,7 +126,7 @@ const Promise = (()=>{
 
         // 2.3.1 
         if (_promise2 === _x) {
-            _reject(new TypeError('Chaining cycle detected for promise!'));
+            return _reject(new TypeError('Chaining cycle detected for promise!'));
         }
 
         // 2.3.2 
